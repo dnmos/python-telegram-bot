23212d2e6917d9f61f02424f24b930cb79d54e5c
Dmitry Luzin <dnluzin@gmail.com>
2022-12-24 13:42:18 +0000

    add commands /start and /services

--- testbot.py
+++ testbot.py
@@ -2,18 +2,64 @@
 from __future__ import unicode_literals 
 from flask import Flask, request
 import telegram
-from auth_data import token
-from domain_data import domain_name
+from credentials import token, domain_name
 
 app = Flask(__name__)
 app.debug = True
 
 TOKEN = token
+URL = domain_name
 
 global bot 
 bot = telegram.Bot(token=TOKEN)
 
-URL = domain_name
+text_start = \
+	"Привет! Это бот сайта \nvisitbudapest.ru\n\n"\
+	"Бронировать путешествия в Будапеште по лучшим ценам \n/services\n\n"\
+	"Скачать статью в PDF \n/pdf"
+
+booking_links = {
+	"aviasales"       : "https://aviasales.tp.st/UjGdnswh",
+	"ostrovok"        : "https://ostrovok.tp.st/AAuVl11s",
+	"budapest-card"   : "https://getyourguide.tp.st/lb1gHBuo",
+	"music"           : "https://getyourguide.tp.st/FPLOTyuR",
+	"hop-on-hop-off"  : "https://getyourguide.tp.st/9XbNgvn4",
+	"szechenyi" : "https://getyourguide.tp.st/8BJxp83p",
+	"cruises"         : "https://getyourguide.tp.st/yl94pXIX",
+	"szentendre"      : "https://tripster.tp.st/FWI4AlO4",
+	"cherehapa"       : "https://cherehapa.tp.st/sBWDO7e5",
+	"auto"            : "https://economybookings.tp.st/3gksCIJ2",
+	"kiwitaxi"        : "https://kiwitaxi.tp.st/mucJFLwZ",
+	"tripster"        : "https://tripster.tp.st/mojAh9jY",
+	"level"           : "https://level.tp.st/1kTUYtSy",
+	"sanatoriums"     : "https://sanatoriums.tp.st/OASrIX6y",
+	"getyourguide"    : "https://getyourguide.tp.st/eGqlK7Rp",
+	"drimsim"         : "https://drimsim.tp.st/XsVNrhNk",
+}
+
+text_services = \
+	f"Авиабилеты <a href='{booking_links['aviasales']}'>aviasales</a>\n\n"\
+	f"Отели <a href='{booking_links['ostrovok']}'>ostrovok</a>\n\n"\
+	f"BudapestCard <a href='{booking_links['budapest-card']}'>budapest-card</a>\n\n"\
+	f"Мюзиклы и шоу <a href='{booking_links['music']}'>music</a>\n\n"\
+	f"Hop On Hop Off <a href='{booking_links['hop-on-hop-off']}'>hop-on-hop-off</a>\n\n"\
+	f"Купальня Сечени <a href='{booking_links['szechenyi']}'>szechenyi</a>\n\n"\
+	f"Круизы по Дунаю <a href='{booking_links['cruises']}'>cruises</a>\n\n"\
+	f"Сентендре <a href='{booking_links['szentendre']}'>szentendre</a>\n\n"\
+	f"Страховка <a href='{booking_links['cherehapa']}'>cherehapa</a>\n\n"\
+	f"Аренда авто <a href='{booking_links['auto']}'>auto</a>\n\n"\
+	f"Трансферы <a href='{booking_links['kiwitaxi']}'>kiwitaxi</a>\n\n"\
+	f"Экскурсии <a href='{booking_links['tripster']}'>tripster</a>\n\n"\
+	f"Туры <a href='{booking_links['level']}'>level</a>\n\n"\
+	f"Санатории <a href='{booking_links['sanatoriums']}'>sanatoriums</a>\n\n"\
+	f"Входные билеты <a href='{booking_links['getyourguide']}'>getyourguide</a>\n\n"\
+	f"Сим-карта <a href='{booking_links['drimsim']}'>drimsim</a>\n\n"
+
+directory = "https://visitbudapest.ru/wp-content/uploads/pdf/"
+
+articles = {
+	"christmas-markets": "",
+}
 
 #WebHook
 @app.route('/HOOK', methods=['POST', 'GET']) 
@@ -22,10 +68,15 @@ def webhook_handler():
 		update = telegram.Update.de_json(request.get_json(force=True), bot)
 		try:
 			chat_id = update.message.chat.id 
+			msg_id = update.message.message_id
 			text = update.message.text
 			userid = update.message.from_user.id
 			username = update.message.from_user.username
-			bot.send_message(chat_id=chat_id, text="hello")
+			userfullname = update.message.from_user.full_name
+			if text == "/start":
+				bot.sendMessage(chat_id=chat_id, text=text_start)
+			elif text == "/services":
+				bot.sendMessage(chat_id=chat_id, parse_mode="HTML", text=text_services, disable_web_page_preview=True)
 		except Exception as e:
 			print(e)
 	return 'ok' 
@@ -43,4 +94,3 @@ def set_webhook():
 @app.route("/")
 def hello():
 	return "Hello"
-
