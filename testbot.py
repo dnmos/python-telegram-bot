0e495d2650f1a106ba8880a6fa2b6ee761c40006
Dmitry Luzin <dnluzin@gmail.com>
2023-01-06 12:53:29 +0000

    add tools.py

--- testbot.py
+++ testbot.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from flask import Flask, request
 import telegram
 from credentials import token, domain_name
+from tools import booking_links
 
 app = Flask(__name__)
 app.debug = True
@@ -18,42 +19,11 @@ text_start = \
 	"Бронировать путешествия в Будапеште по лучшим ценам \n/services\n\n"\
 	"Скачать статью в PDF \n/pdf"
 
-booking_links = {
-	"aviasales"       : "https://aviasales.tp.st/UjGdnswh",
-	"ostrovok"        : "https://ostrovok.tp.st/AAuVl11s",
-	"budapest-card"   : "https://getyourguide.tp.st/lb1gHBuo",
-	"music"           : "https://getyourguide.tp.st/FPLOTyuR",
-	"hop-on-hop-off"  : "https://getyourguide.tp.st/9XbNgvn4",
-	"szechenyi" : "https://getyourguide.tp.st/8BJxp83p",
-	"cruises"         : "https://getyourguide.tp.st/yl94pXIX",
-	"szentendre"      : "https://tripster.tp.st/FWI4AlO4",
-	"cherehapa"       : "https://cherehapa.tp.st/sBWDO7e5",
-	"auto"            : "https://economybookings.tp.st/3gksCIJ2",
-	"kiwitaxi"        : "https://kiwitaxi.tp.st/mucJFLwZ",
-	"tripster"        : "https://tripster.tp.st/mojAh9jY",
-	"level"           : "https://level.tp.st/1kTUYtSy",
-	"sanatoriums"     : "https://sanatoriums.tp.st/OASrIX6y",
-	"getyourguide"    : "https://getyourguide.tp.st/eGqlK7Rp",
-	"drimsim"         : "https://drimsim.tp.st/XsVNrhNk",
-}
-
-text_services = \
-	f"Авиабилеты <a href='{booking_links['aviasales']}'>aviasales</a>\n\n"\
-	f"Отели <a href='{booking_links['ostrovok']}'>ostrovok</a>\n\n"\
-	f"BudapestCard <a href='{booking_links['budapest-card']}'>budapest-card</a>\n\n"\
-	f"Мюзиклы и шоу <a href='{booking_links['music']}'>music</a>\n\n"\
-	f"Hop On Hop Off <a href='{booking_links['hop-on-hop-off']}'>hop-on-hop-off</a>\n\n"\
-	f"Купальня Сечени <a href='{booking_links['szechenyi']}'>szechenyi</a>\n\n"\
-	f"Круизы по Дунаю <a href='{booking_links['cruises']}'>cruises</a>\n\n"\
-	f"Сентендре <a href='{booking_links['szentendre']}'>szentendre</a>\n\n"\
-	f"Страховка <a href='{booking_links['cherehapa']}'>cherehapa</a>\n\n"\
-	f"Аренда авто <a href='{booking_links['auto']}'>auto</a>\n\n"\
-	f"Трансферы <a href='{booking_links['kiwitaxi']}'>kiwitaxi</a>\n\n"\
-	f"Экскурсии <a href='{booking_links['tripster']}'>tripster</a>\n\n"\
-	f"Туры <a href='{booking_links['level']}'>level</a>\n\n"\
-	f"Санатории <a href='{booking_links['sanatoriums']}'>sanatoriums</a>\n\n"\
-	f"Входные билеты <a href='{booking_links['getyourguide']}'>getyourguide</a>\n\n"\
-	f"Сим-карта <a href='{booking_links['drimsim']}'>drimsim</a>\n\n"
+text_services = ""
+for link in booking_links:
+	text = booking_links[link]["text"]
+	url = booking_links[link]["url"]
+	text_services = text_services + f"{text} <a href='{url}'>{link}</a>\n\n"
 
 directory = "https://visitbudapest.ru/wp-content/uploads/pdf/"
 
